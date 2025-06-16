3daec7cbc8b3b2719ce35546a3884df3c6eb244b
CosmicFabri <fabrizzio86@outlook.es>
2025-06-16 14:12:22 -0600

    feat: Added basic structure for the Equipos shop view; improved loading screen.

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -18,23 +18,21 @@ onMounted(async() => {
 </script>
 
 <template>
-    <div class="flex flex-col lg:px-20 px-14 mt-14 lg:gap-y-16 gap-y-10 2xl:mb-0 mb-12">
-        <!-- Loading screen -->
+    <!-- Loading screen -->
+    <div v-if="isLoading" class="flex flex-col mx-auto my-auto items-center gap-y-8 mt-24">
         <div
-            v-if="isLoading"
-            class="flex flex-col mx-auto my-auto items-center gap-y-8 mt-24">
-            <div
-                class="text-sky-600 pi pi-spinner-dotted animate-spin slow-spin"
-                style="font-size: 8rem">
-            </div>
-            <div class="lg:text-5xl text-4xl text-sky-700 font-bold">
-                Cargando...
-            </div>
+            class="text-sky-600 pi pi-spinner-dotted animate-spin slow-spin"
+            style="font-size: 8rem">
         </div>
+        <div class="lg:text-5xl text-4xl text-sky-700 font-bold">
+            Cargando...
+        </div>
+    </div>
 
+    <!-- Content -->
+    <div v-else class="flex flex-col lg:px-20 px-14 mt-14 lg:gap-y-16 gap-y-10 2xl:mb-0 mb-12">
         <!-- Title and description -->
         <div
-            v-else
             class="flex flex-col gap-y-8">
             <div class="md:text-6xl text-4xl font-semibold text-sky-800 -mb-4">
                 Tienda
@@ -50,7 +48,7 @@ onMounted(async() => {
                         <img
                             :src="`src/assets/images/shop/${index}.jpg`"
                             class="size-60 border-solid border-4 border-sky-600 rounded-xl shadow-lg
-                                   hover:shadow-2xl transform transition duration-200 hover:scale-105"
+                                    hover:shadow-2xl transform transition duration-200 hover:scale-105"
                         >
                         <div class="font-semibold text-2xl text-sky-800">{{ product.name }}</div>
                     </div>
--- src/views/shop/EquiposView.vue
+++ src/views/shop/EquiposView.vue
@@ -1,8 +1,41 @@
 <script setup>
+import { fetchData } from '@/utils/api';
+import { ref } from 'vue';
+import { onMounted } from 'vue';
+
+const isLoading = ref(true)
+const equipos = ref([])
+
+// Retrieve all the 'Equipos' labeled products
+onMounted(async () => {
+    try {
+        const response = await fetchData('equipos', 'GET')
+
+        if (response) {
+            isLoading.value = false
+            equipos.value = response
+        }
+        
+    } catch (error) {
+        throw error
+    }
+})
 </script>
 
 <template>
-    <div class="flex flex-col lg:px-20 px-14 mt-14 lg:gap-y-16 gap-y-10 2xl:mb-0 mb-12">
+    <!-- Loading screen -->
+    <div v-if="isLoading" class="flex flex-col mx-auto my-auto items-center gap-y-8 mt-24">
+        <div
+            class="text-sky-600 pi pi-spinner-dotted animate-spin slow-spin"
+            style="font-size: 8rem">
+        </div>
+        <div class="lg:text-5xl text-4xl text-sky-700 font-bold">
+            Cargando...
+        </div>
+    </div>
+
+    <!-- Content -->
+    <div v-else class="flex flex-col lg:px-20 px-14 mt-14 lg:gap-y-16 gap-y-10 2xl:mb-0 mb-12">
         <!-- Title and description -->
         <div class="flex flex-col gap-y-8">
             <div class="md:text-6xl text-4xl font-semibold text-sky-800 -mb-4">
@@ -12,13 +45,15 @@
                 Explore el catálogo de productos
             </div>
         </div>
-
+        
+        <!-- Product grid (list, on phone) -->
         <div class="flex justify-center">
             <div class="lg:grid lg:grid-cols-3 lg:gap-x-12 flex-col">
-                <div class="bg-sky-600 rounded-xl shadow-xl">1</div>
-                <div class="bg-sky-600 rounded-xl shadow-xl">2</div>
-                <div class="bg-sky-600 rounded-xl shadow-xl">3</div>
-                <div class="bg-sky-600 rounded-xl shadow-xl">4</div>
+                <div
+                    v-for="(equipo, index) in equipos" :key="index"
+                    class="bg-sky-300 rounded-xl shadow-xl">
+                    {{ equipo.name }}
+                </div>
             </div>
         </div>
     </div>
