aaa256ebdb3d16f23d7156761995519d6f1dceda
CosmicFabri <fabrizzio86@outlook.es>
2025-06-16 16:39:18 -0600

    fix: Fixed the layout of the Equipos products for phones.

--- src/views/shop/EquiposView.vue
+++ src/views/shop/EquiposView.vue
@@ -48,16 +48,16 @@ onMounted(async () => {
         
         <!-- Product grid (list, on phone) -->
         <div class="flex justify-center">
-            <div class="lg:grid lg:grid-cols-4 lg:gap-x-12 lg:gap-y-10 flex flex-col gap-y-8 text-center">
+            <div class="grid lg:grid-cols-4 grid-cols-2 lg:gap-x-12 gap-x-8 lg:gap-y-10 gap-y-8 text-center">
                 <div v-for="(equipo, index) in equipos" :key="index">
                     <RouterLink :to="{ name: 'product', params: { id: equipo.id} }" :productID="equipo.code">
                         <div class="flex flex-col items-center gap-y-4">
                             <img
                                 :src="`/src/assets/images/shop/products/equipos/${equipo.code}.jpeg`"
-                                class="size-60 border-solid border-4 border-sky-600 rounded-xl shadow-lg
+                                class="lg:size-60 size-40 border-solid border-4 border-sky-600 rounded-xl shadow-lg
                                         hover:shadow-2xl transform transition duration-200 hover:scale-105"
                             >
-                            <div class="font-semibold text-lg text-sky-800">
+                            <div class="font-semibold lg:text-lg text-md text-sky-800">
                                 {{ equipo.name }}
                             </div>
                         </div>
