498e8e80135e0d91fe068c1956dc0ae37b9573f6
CosmicFabri <fabrizzio86@outlook.es>
2025-06-16 10:29:54 -0600

    feat: Added four optical equipment product images, created basic structure for the equipos shop view.

--- src/views/shop/EquiposView.vue
+++ src/views/shop/EquiposView.vue
@@ -2,7 +2,24 @@
 </script>
 
 <template>
-    <div class="text-xl font-semibold">
-        This is the Equipos view.
+    <div class="flex flex-col lg:px-20 px-14 mt-14 lg:gap-y-16 gap-y-10 2xl:mb-0 mb-12">
+        <!-- Title and description -->
+        <div class="flex flex-col gap-y-8">
+            <div class="md:text-6xl text-4xl font-semibold text-sky-800 -mb-4">
+                Equipos ópticos
+            </div>
+            <div class="text-lg font-medium text-sky-800">
+                Explore el catálogo de productos
+            </div>
+        </div>
+
+        <div class="flex justify-center">
+            <div class="lg:grid lg:grid-cols-3 lg:gap-x-12 flex-col">
+                <div class="bg-sky-600 rounded-xl shadow-xl">1</div>
+                <div class="bg-sky-600 rounded-xl shadow-xl">2</div>
+                <div class="bg-sky-600 rounded-xl shadow-xl">3</div>
+                <div class="bg-sky-600 rounded-xl shadow-xl">4</div>
+            </div>
+        </div>
     </div>
 </template>
